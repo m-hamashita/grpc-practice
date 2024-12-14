8651be51286a5669847b203b93ab57814888cee6
m-hamashita <masakatsu.hamashita@legalontech.jp>
2024-12-15 04:27:51 +0900

    Add GetModel rpc

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,5 @@
 use sample::sample_client::SampleClient;
-use sample::GetCounterRequest;
+use sample::{GetCounterRequest, GetModelRequest};
 
 pub mod sample {
     tonic::include_proto!("sample");
@@ -10,10 +10,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = SampleClient::connect("http://[::1]:50051").await?;
 
     for _ in 0..10 {
-        let request = tonic::Request::new(GetCounterRequest {});
-        let response = client.get_counter(request).await?;
+        // let request = tonic::Request::new(GetCounterRequest {});
+        // let response = client.get_counter(request).await?;
+        // println!("Counter value: {}", response.into_inner().value);
 
-        println!("Counter value: {}", response.into_inner().value);
+        let request = tonic::Request::new(GetModelRequest {});
+        let response = client.get_model(request).await?;
+        println!("Model: {:?}", response.into_inner());
 
         tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
     }
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,36 @@ use std::sync::{Arc, Mutex};
 use tonic::{transport::Server, Request, Response, Status};
 use tokio::time::{sleep, Duration};
 use sample::sample_server::{Sample, SampleServer};
-use sample::{GetCounterRequest, GetCounterResponse};
+use sample::{GetCounterRequest, GetCounterResponse, GetModelRequest, GetModelResponse};
 
 pub mod sample {
     tonic::include_proto!("sample");
 }
 
 #[derive(Debug)]
+pub struct Model {
+    name: String,
+    data: String,
+}
+
+#[derive(Debug)]
 pub struct SampleService {
     counter: Arc<Mutex<i32>>,
+    model: Arc<Mutex<Model>>,
 }
 
 #[tonic::async_trait]
 impl Sample for SampleService {
+    async fn get_model(
+        &self,
+        _request: Request<GetModelRequest>,
+    ) -> Result<Response<GetModelResponse>, Status> {
+        let model = self.model.lock().unwrap();
+        Ok(Response::new(GetModelResponse {
+            name: model.name.clone(),
+            data: model.data.clone(),
+        }))
+    }
     async fn get_counter(
         &self,
         _request: Request<GetCounterRequest>,
@@ -26,21 +43,32 @@ impl Sample for SampleService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let model = Arc::new(Mutex::new(Model {
+        name: "Sample".to_string(),
+        data: "sample data".to_string(),
+    }));
     let counter = Arc::new(Mutex::new(0));
 
     let counter_clone = Arc::clone(&counter);
+    let model_clone = Arc::clone(&model);
     tokio::spawn(async move {
         loop {
             {
                 let mut num = counter_clone.lock().unwrap();
                 *num += 1;
+                let new_model = Model {
+                    name: format!("Model {}", num),
+                    data: "sample data".to_string(),
+                };
+                let mut model = model_clone.lock().unwrap();
+                *model = new_model;
             }
             sleep(Duration::from_secs(5)).await;
         }
     });
 
     let addr = "[::1]:50051".parse()?;
-    let sample_service = SampleService { counter };
+    let sample_service = SampleService { counter, model };
 
     println!("gRPC server listening on {}", addr);
 
